3d68a516bc62d9729445e67190553b3ecd3d3d2e
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-06-06 11:51:50 -0300

    adding new elements and links

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import App from "./App";
 import Profile from "./components/Profile";
 import Info from "./components/Info";
@@ -8,10 +8,19 @@ const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <App />
+              <Link to="info">Info</Link>
+              <div>This is a page</div>
+            </div>
+          }
+        />
         <Route path="/profile" element={<Profile />} />
         <Route path="/info" element={<Info />} />
-
+        <Route path="/about" element={<div>About</div>} />
       </Routes>
     </BrowserRouter>
   );
